0cd3e88749baa34405331e084f0118cce09cd769
selfup <boudinot.regis@yahoo.com>
2016-01-13 15:05:38 -0700

    all hooked up

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
+const http = require('http')
 const express = require('express')
+const socketIo = require('socket.io')
 const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const path = require('path')
+const port = process.env.PORT || 3000
+
+const server = http.createServer(app)
+.listen(port, () => {
+  console.log('Listening on port ' + port + '.')
+})
+
+const io = socketIo(server)
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/polls', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/live_poll.html'))
+  res.sendFile(__dirname + '/public/live_poll.html')
 })
 
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/admin_poll.html'))
+  res.sendFile(__dirname + '/public/admin_poll.html')
 })
 
-http.listen(process.env.PORT || 3000, () => {
-  console.log('Your server is up and running on Port 3000. Good job!')
+app.get('/student_poll', (req, res) => {
+  res.sendFile(__dirname + '/public/student_poll.html')
 })
+
+
+io.on('connection', (socket) => {
+  io.sockets.emit('usersConnected', io.engine.clientsCount)
+  socket.emit('statusMessage', 'You have connected.')
+
+  socket.on('message', (channel, message) => {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      socket.emit('userVote', message)
+      socket.emit('voteCount', countVotes(votes))
+    }
+  })
+
+  socket.on('disconnect', () => {
+    delete votes[socket.id]
+    io.sockets.emit('usersConnected', io.engine.clientsCount)
+  })
+})
+
+const votes = {}
+
+const countVotes = (votes) => {
+  const voteCount = {
+      A: 0,
+      B: 0,
+      C: 0,
+      D: 0
+  }
+  for (vote in votes) {
+    voteCount[votes[vote]]++
+  }
+  return voteCount;
+}
+
+module.exports = server
