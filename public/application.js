cf6aaff1374bc086510eac8eb9756fb18da286bd
selfup <boudinot.regis@yahoo.com>
2016-01-12 19:57:33 -0700

    refactored application.js

--- public/application.js
+++ public/application.js
@@ -6,11 +6,7 @@ $('.submit-live-poll').on('click', (e) => {
   var thirdQuality = $('.new-live-poll .t-q').val()
 
   $('.live-poll').append(
-    `<div>
-      <h3>${firstQuality}</h3>
-      <h3>${secondQuality}</h3>
-      <h3>${thirdQuality}</h3>
-    </div>`
+    pollQualities(firstQuality, secondQuality, thirdQuality)
   )
 })
 
@@ -22,10 +18,14 @@ $('.submit-admin-poll').on('click', (e) => {
   var thirdQuality = $('.new-admin-poll .t-q').val()
 
   $('.admin-poll').append(
-    `<div>
-      <h3>${firstQuality}</h3>
-      <h3>${secondQuality}</h3>
-      <h3>${thirdQuality}</h3>
-    </div>`
+    pollQualities(firstQuality, secondQuality, thirdQuality)
   )
 })
+
+const pollQualities = (firstQ, secondQ, thirdQ) => {
+  return `<div>
+    <h3>${firstQ}</h3>
+    <h3>${secondQ}</h3>
+    <h3>${thirdQ}</h3>
+  </div>`
+}
